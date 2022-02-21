9fc27311f2a2c509ded16a8436f22dd39033b4ce
Kamdoum <kamdoumloich@yahoo.fr>
2022-02-21 01:36:45 +0100

    with List and Row

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,66 @@ class MyApplication extends StatelessWidget {
             children: [
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: Image('/images/detective.png'),
+                backgroundImage: AssetImage('images/detective.png'),
+              ),
+              Text('Loich Kamdoum Deameni',
+              style: TextStyle(
+                fontFamily: 'UnifrakturCook',
+                fontSize: 25.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              Text('Data scientist',
+              style: TextStyle(
+                fontFamily: 'Julee',
+                fontSize: 25.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.green,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('+49 151 6658332X',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.green,
+                      ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail_outline,
+                      color: Colors.green,
+                    ),
+                    title: Text('kamdoumloic@gmail.com',
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
